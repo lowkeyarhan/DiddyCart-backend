69c43680ead74b097060486d601a0a76e251337f
lowkeyarhan <anup.arhan.das@gmail.com>
2026-01-17 15:06:16 +0530

    First commit

---
+++ src/main/java/com/diddycart/app/DiddycartApplication.java
@@ -0,0 +1,13 @@
+package com.diddycart.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiddycartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiddycartApplication.class, args);
+	}
+
+}
