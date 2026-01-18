090e6346c1c30ef14d87c1e4ae839a503e9f62a6
lowkeyarhan <anup.arhan.das@gmail.com>
2026-01-19 02:07:52 +0530

    Remove unused import statement from OrderService

--- src/main/java/com/diddycart/service/OrderService.java
+++ src/main/java/com/diddycart/service/OrderService.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
